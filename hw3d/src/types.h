4c4811ad33146762dd04226150464fff105765cd
joão f mota <112226912+omzandra@users.noreply.github.com>
2022-09-05 16:12:18 +0100

    Update README.md

---
+++ hw3d/src/types.h
@@ -0,0 +1,17 @@
+#ifndef TYPES_H
+#define TYPES_H
+
+typedef signed char        s8;
+typedef unsigned char      u8;
+typedef short              s16;
+typedef unsigned short     u16;
+typedef int                s32;
+typedef unsigned int       u32;
+typedef long long          s64;
+typedef unsigned long long u64;
+
+typedef float			   r32;
+typedef double			   r64;
+
+#endif // !TYPES_H
+
